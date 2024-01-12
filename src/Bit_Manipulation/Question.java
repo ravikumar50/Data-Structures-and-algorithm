bc2f21397baf122072fc0ded61680e5c274fbd56
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-01-12 22:42:32 +0530

    Number of Set Bits Problem Added

--- src/Bit_Manipulation/Question.java
+++ src/Bit_Manipulation/Question.java
@@ -6,13 +6,13 @@ public class Question {
         int ans = 0;
 
         while(n!=0){
-            if(n%2==1) ans++;
-            n = n>>1;
+            n = n & (n-1);     // see interesting facts 1
+            ans++;
         }
         return ans;
     }
     public static void main(String[] args) {
-        int n = 7;
+        int n = 10;
         int ans = number_of_set_bits(n);
         System.out.println(ans);
     }
